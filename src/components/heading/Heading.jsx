b43353ce6b99d85b176dd8a1bc9c411fd7046704
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-09-22 12:59:32 +0300

    Heading creation

--- src/components/heading/Heading.jsx
+++ src/components/heading/Heading.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import logo from '../images/logo-minimal.png';
 
 const Heading = () => {
   return (
     <div>
       <header className="header"></header>
+      <div className="logo-container">
+        <img
+          className="logo"
+          src={logo}
+          alt="logo of the site 'Arminas photography'"
+        />
+      </div>
+      <h1 className="heading-container">
+        <span className="heading-main">Photography</span>
+        <span className="heading-sub">By Arminas Strolys</span>
+      </h1>
     </div>
   );
 };
