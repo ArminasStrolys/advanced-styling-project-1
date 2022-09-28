a4454481efc9bba4716f2dce2f75ee9cd972eb7d
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-09-28 19:32:33 +0300

    Gradient text

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Intro = () => {
-  return <main>Test</main>;
+  return (
+    <main>
+      <section className="intro">
+        <h2 className="heading-secondary">Photo and editing skills</h2>
+      </section>
+    </main>
+  );
 };
 
 export default Intro;
