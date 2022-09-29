98cacb25f8ace45c8d7140ccc55b05a11eb3dcc1
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-09-29 17:18:18 +0300

    Next section at work

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -4,7 +4,41 @@ const Intro = () => {
   return (
     <main>
       <section className="intro">
-        <h2 className="heading-secondary">Photo and editing skills</h2>
+        <h2 className="heading-secondary margin-XL">
+          Photo and editing skills
+        </h2>
+        <div className="row">
+          <div className="col-1-of-2">
+            <h3 className="heading-tertiary margin-S">
+              Photography skills of a master
+            </h3>
+            <p className="paragraph">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga,
+              debitis esse mollitia, perferendis repellat explicabo, quibusdam
+              illum maxime magni laborum tempore rerum non veritatis ipsum
+              quidem alias incidunt eligendi saepe!
+            </p>
+            <h3 className="heading-tertiary margin-S">
+              Lightroom to customize the shades
+            </h3>
+            <p className="paragraph">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga,
+              debitis esse mollitia, perferendis repellat explicabo, quibusdam
+              illum maxime magni laborum tempore rerum non veritatis ipsum
+              quidem alias incidunt eligendi saepe!
+            </p>
+            <h3 className="heading-tertiary margin-S">
+              Photoshop to adjust inperfect details
+            </h3>
+            <p className="paragraph">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga,
+              debitis esse mollitia, perferendis repellat explicabo, quibusdam
+              illum maxime magni laborum tempore rerum non veritatis ipsum
+              quidem alias incidunt eligendi saepe!
+            </p>
+          </div>
+          <div className="col-1-of-2">Test Text TTTTTTTTTTTTTTTTT</div>
+        </div>
       </section>
     </main>
   );
