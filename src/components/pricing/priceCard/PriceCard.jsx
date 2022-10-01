78d1661825849beb87d85e24662a42b76bb43486
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-10-01 21:55:23 +0300

    Adjusting cards

--- src/components/pricing/priceCard/PriceCard.jsx
+++ src/components/pricing/priceCard/PriceCard.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 
 const PriceCard = (props) => {
   return (
-    <div>
-      <p>Plan</p>
-      <p>{props.info}</p>
-      <p>Plan: {props.plan}</p>
+    <div className="col-1-of-3">
+      <div className="card">
+        <div className="card__side card__side--front">
+          <p>Plan</p>
+          <p>{props.info}</p>
+          <p>Plan: {props.plan}</p>
+        </div>
+        <div className="card__side card__side--back">
+          <p>Only 99$</p>
+        </div>
+      </div>
     </div>
   );
 };
