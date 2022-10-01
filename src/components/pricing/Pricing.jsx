aa089a0c4496f275a246f2ee198f550aa451f8ea
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-10-01 22:25:50 +0300

    Image adjustment

--- src/components/pricing/Pricing.jsx
+++ src/components/pricing/Pricing.jsx
@@ -2,9 +2,24 @@ import React from 'react';
 import PriceCard from './priceCard/PriceCard';
 
 const data = [
-  { text: 'this is sample text', plan: 'Basic', index: 15 },
-  { text: 'this is sample text', plan: 'Plus', index: 15 },
-  { text: 'this is sample text', plan: 'Premium', index: 15 },
+  {
+    heading: 'Polaroid experience',
+    text: 'this is sample text',
+    plan: 'Basic',
+    price: 40,
+  },
+  {
+    heading: 'Picture experience',
+    text: 'this is sample text',
+    plan: 'Plus',
+    price: 99,
+  },
+  {
+    heading: 'Photoshoot experience',
+    text: 'this is sample text',
+    plan: 'Premium',
+    price: 300,
+  },
 ];
 
 const Pricing = () => {
@@ -13,7 +28,14 @@ const Pricing = () => {
       <h2 className="heading-secondary margin-XL">Service packages</h2>
       <div className="row">
         {data.map((e, index) => (
-          <PriceCard key={index} info={e.text} plan={e.plan} />
+          <PriceCard
+            key={index}
+            info={e.text}
+            plan={e.plan}
+            heading={e.heading}
+            price={e.price}
+            cardNum={index + 1}
+          />
         ))}
       </div>
     </section>
--- src/components/pricing/priceCard/PriceCard.jsx
+++ src/components/pricing/priceCard/PriceCard.jsx
@@ -3,14 +3,17 @@ import React from 'react';
 const PriceCard = (props) => {
   return (
     <div className="col-1-of-3">
-      <div className="card">
+      <div className={`card card--${props.cardNum}`}>
         <div className="card__side card__side--front">
+          <div className="card__picture"></div>
+          <div className="card__heading">{props.heading}</div>
+          <div className="card__details"></div>
           <p>Plan</p>
           <p>{props.info}</p>
           <p>Plan: {props.plan}</p>
         </div>
         <div className="card__side card__side--back">
-          <p>Only 99$</p>
+          <p>Only {props.price}$</p>
         </div>
       </div>
     </div>
