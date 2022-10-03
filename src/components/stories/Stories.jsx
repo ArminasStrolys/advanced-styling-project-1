4ddb744587a72e0663d3bfa6ae26d4328f0bc6c5
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-10-03 22:08:45 +0300

    A few comments

--- src/components/stories/Stories.jsx
+++ src/components/stories/Stories.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import avatar1 from '../images/avatar-1.jpg';
+import avatar2 from '../images/avatar-2.jpg';
 
 const Stories = () => {
   return (
@@ -6,10 +8,32 @@ const Stories = () => {
       <h2 className="heading-secondary margin-XL">What others have to say</h2>
       <div className="row">
         <div className="story">
-          <figure className="story__shape"></figure>
+          <figure className="story__shape">
+            <img className="story__image" src={avatar1} alt="" />
+          </figure>
           <div className="story__text">
             <h3 className="heading-tertiary margin-S">
               Those photos are incredible!!!
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime,
+              consequuntur! Officia error dolore impedit temporibus repellendus
+              cumque maxime tempore sequi aliquam, voluptatum id possimus itaque
+              asperiores, assumenda harum ipsum inventore blanditiis ratione
+              officiis magnam perspiciatis sint animi quam. Quisquam veniam, rem
+              quasi, nisi, at ea similique dicta explicabo voluptas quod illum
+              asperiores consequuntur dolore necessitatibus sunt tempora dolorum
+              ratione?
+            </p>
+          </div>
+        </div>
+        <div className="story">
+          <figure className="story__shape">
+            <img className="story__image" src={avatar2} alt="" />
+          </figure>
+          <div className="story__text">
+            <h3 className="heading-tertiary margin-S">
+              Wow. The quality and service times was fenomenal.
             </h3>
             <p>
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime,
