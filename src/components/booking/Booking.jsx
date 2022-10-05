96c1419b5f400e2c22a8ecdc63f5ea3e81c4b219
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-10-05 17:11:05 +0300

    Subscribe form

--- src/components/booking/Booking.jsx
+++ src/components/booking/Booking.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Booking = () => {
-  return <section className="section-booking">TEST</section>;
+  return (
+    <section className="section-booking">
+      <div className="row">
+        <div className="book"></div>
+      </div>
+    </section>
+  );
 };
 
 export default Booking;
