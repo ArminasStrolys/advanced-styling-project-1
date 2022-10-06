44e81ff996da3d9df21336fda57dbc8b436edf73
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-10-06 21:38:57 +0300

    Open n close button done

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -10,7 +10,7 @@ const Navigation = () => {
           id="navi-toggle"
         />
         <label htmlFor="navi-toggle" className="navigation__button">
-          MENU
+          <span className="navigation__icon">&nbsp;</span>
         </label>
         <div className="navigation__background">&nbsp;</div>
         <nav className="navigation__nav">
