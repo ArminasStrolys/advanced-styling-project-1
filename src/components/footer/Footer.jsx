67e3289f55a43ef5802b86f813bf2bf6cfc8f189
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-10-06 17:17:54 +0300

    Footer layout

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,57 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <footer className="footer">
+        <div className="footer__logo-box">
+          <img src="" alt="" className="footer__logo" />
+        </div>
+        <div className="row">
+          <div className="col-1-of-2">
+            <div className="footer__navigation">
+              <ul className="footer__list">
+                <li className="footer__item">
+                  <a href="#" className="footer__link">
+                    Company
+                  </a>
+                </li>
+                <li className="footer__item">
+                  <a href="#" className="footer__link">
+                    Contact us
+                  </a>
+                </li>
+                <li className="footer__item">
+                  <a href="" className="footer__link">
+                    Career
+                  </a>
+                </li>
+                <li className="footer__item">
+                  <a href="#" className="footer__link">
+                    Privacy policy
+                  </a>
+                </li>
+                <li className="footer__item">
+                  <a href="#" className="footer__link">
+                    Terms
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-1-of-2">
+            <p className="footer__copyright">
+              Web built by{' '}
+              <a href="#" className="footer__link">
+                Arminas Strolys. Copyright &copy; 2022.
+              </a>
+            </p>
+          </div>
+        </div>
+      </footer>
+      ;
+    </>
+  );
 };
 
 export default Footer;
